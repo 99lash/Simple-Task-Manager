0b078a03a73b441cc65c8068a95da6ddd62ca187
Asher <asherjohn48@gmail.com>
2025-07-22 23:31:52 +0800

    remove boilerplates

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<template>
+  <header>
+    <nav className="navbar">
+      <div className="navbar-start">
+        <a href="/" className="text-3xl font-bold">Simple Task Manager</a>
+      </div>
+      <div className="navbar-end">
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn m-1">
+            <span id="theme-name">Theme: Default</span>
+            <svg
+              width="12px"
+              height="12px"
+              class="inline-block h-2 w-2 fill-current opacity-60"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048"
+            >
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+          <ul tabindex="0" class="dropdown-content bg-base-300 max-w-40 w-40 z-1 p-2 shadow-2xl">
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Default"
+                value="default"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Retro"
+                value="retro"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Cyberpunk"
+                value="cyberpunk"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Valentine"
+                value="valentine"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label="Aqua"
+                value="aqua"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<script>
+import AppHeader from '../components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<template>
+  <!-- <div className="mx-20 my-auto"> -->
+  <AppHeader />
+  <!-- </div> -->
+</template>
