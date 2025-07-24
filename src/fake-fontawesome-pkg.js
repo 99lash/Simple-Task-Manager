1c7299517d805c14acee7617782f27b45a47ffe8
Asher <asherjohn48@gmail.com>
2025-07-24 20:20:40 +0800

    resolve alias

---
+++ src/fake-fontawesome-pkg.js
@@ -0,0 +1,3 @@
+export default {
+  version: '7.0.0',
+};
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import fontawesomPkg from '@fortawesome/fontawesome-svg-core';
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    __FA_VERSION__: JSON.stringify(fontawesomPkg.version),
-  },
   plugins: [
     vue(),
     vueDevTools(),
@@ -18,7 +14,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@fortawesome/fontawesome-svg-core/package.json': '/src/fake-fontawesome-pkg.js',
     },
   },
 });
