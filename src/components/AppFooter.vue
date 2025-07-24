8a9dfda9eb909233942c17fb3371176ec7dd298a
Asher <asherjohn48@gmail.com>
2025-07-24 18:51:35 +0800

    added fontawesome icons

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer className="bg-neutral text-neutral-content mt-12">
+    <div className="footer footer-horizontal justify-between container mx-auto p-6 items-center">
+      <aside>
+        <p>
+          Made with ❤️ by
+          <a href="https://github.com/99lash"> 99lash </a>
+        </p>
+      </aside>
+      <nav class="grid-flow-col gap-4">
+        <a href="https://github.com/99lash">
+          <font-awesome-icon icon="fa-brands fa-github" size="xl" />
+        </a>
+        <a href="https://github.com/99lash">
+          <font-awesome-icon icon="fa-brands fa-instagram" size="xl" />
+        </a>
+      </nav>
+    </div>
+  </footer>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-createApp(App).mount('#app');
+library.add(fas, fab);
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
 
 /* theme */
 let currentTheme = localStorage.getItem('theme') || 'default';
