4905c071c5a999482fa2c70babd86bf645fce290
Asher <asherjohn48@gmail.com>
2025-07-24 21:07:06 +0800

    ''

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 // import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { fas } from '@fortawesome/free-solid-svg-icons';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
 
 // library.add(fas, fab);
 
 createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon)
+  // .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
 
 /* theme */
